866f1e78d8ffd4430a20efaf9898b8850dbaf73e
Mark Evans <mrkevans@gmail.com>
2013-10-31 11:26:43 -0700

    Add support for error.body

--- lib/trustedsearch/errors/error.rb
+++ lib/trustedsearch/errors/error.rb
@@ -1,15 +1,19 @@
 module TrustedSearch
   class Error < StandardError
-    attr_reader :message, :code
+    attr_accessor :message, :code, :body
 
-    def initialize(message = nil, code = nil)
+    def initialize(message = nil, code = nil )
+
       @message = message
       @code    = code
+
     end
 
     def to_s
       code_string = code.nil? ? "" : " (Code #{code})"
       "#{message}#{code_string}"
     end
+
+
   end
 end
