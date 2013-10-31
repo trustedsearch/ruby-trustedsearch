ffb0caa6e61188eeeb507b7dc7774561b098ab78
Mark Evans <mrkevans@gmail.com>
2013-10-31 11:26:14 -0700

    correctly encode to hash, return nil if api response is nil

--- lib/trustedsearch/api_response.rb
+++ lib/trustedsearch/api_response.rb
@@ -1,10 +1,13 @@
 module TrustedSearch
   class APIResponse
-    attr_reader :meta, :data, :request
+    attr_reader :meta, :data, :request, :code
 
     def initialize(response)
       @request = response.request
-      @data = response.body.strip.to_json
+      @code = response.code
+      #handle case where body has no response and thus JSON.parse requires octet aka a byte.
+      @data = (response.body.length >=2) ? JSON.parse(response.body.strip) : nil
+
     end
 
   end
